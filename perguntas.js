a7f510835091bdc84d33f432f36786ae2320eaf4
rafaicarvalh0 <160495858+rafaicarvalh0@users.noreply.github.com>
2024-08-26 09:34:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+export const perguntas = [
+    {
+        // enunciado: "Você ja ouviu falar em Aquecimento Global?"
+        alternativas: [ 
+            {
+                texto: "Isso é assustador!",
+                afirmacao: [
+                    "Aquecimento Global nada mais é do que o superaquecimento da terra, que, faz com que a natureza fique toda desregulada.",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Efeito Estufa",
+                afirmacao: [
+                    "Uma consequência desse super aquecimento, que faz da terra uma grande estufa, produzindo calor em excessos."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+];
